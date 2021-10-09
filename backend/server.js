2afb3d7e73c9aecdc5d638fd69e3fa211f32c734
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-10-09 09:31:15 +0100

    newcommit

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,12 @@ app.use('/menu', productRoutes);
 
 const PORT = process.env.PORT || 8000
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
